bd5273856a4cbd08eac46de07ac144bbbbe32949
Tomas1234667 <franciscofigueroaramos4@gmail.com>
2026-05-22 15:31:07 -0600

    fix mysql conflict

--- Jose-Rubio-Ortega-main/backend/db.js
+++ Jose-Rubio-Ortega-main/backend/db.js
@@ -2,15 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-<<<<<<< HEAD
-    port: process.env.DB_PORT
-=======
-    port: 3307
->>>>>>> ad6f0b5055634c92c96965f4874369017b4c5cb8
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 connection.connect((err) => {
